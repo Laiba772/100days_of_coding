5bc7413036866daf1c9e49e1875ac17e492797f3
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2024-04-01 16:23:04 +0500

    Add files via upload

---
+++ Day 2/Q n0 5/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let Famous_person = `Albert Einstein`;
+let message = `"A person who never made a mistake nver tried anythingnew."`;
+console.log(`${Famous_person} once said, ${message}`);
---
+++ Day 2/Q n0 5/app.ts
@@ -0,0 +1,3 @@
+let Famous_person :string = `Albert Einstein`;
+let message :string = `"A person who never made a mistake nver tried anythingnew."`;
+console.log(`${Famous_person} once said, ${message}`)
