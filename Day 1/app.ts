e297e3f3d8b4c05d6715c9c3a5c45b1156c30d80
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2024-04-01 03:55:17 -0700

    Delete Day 1 directory

--- Day 1/app.ts
+++
@@ -1,6 +0,0 @@
-//  Assingment 01.
-
-// Store personName in a variable.
-let personName :string ="Laiba Naz";
-//  print the message.
-console.log(`Hello ${personName}, would you like to learn some pyton today?`)
