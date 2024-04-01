4a651f32a7c3a97509c628c2659eedf0eea224d8
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2024-04-01 15:52:39 +0500

    Add files via upload

---
+++ Day 1/app.ts
@@ -0,0 +1,6 @@
+//  Assingment 01.
+
+// Store personName in a variable.
+let personName :string ="Laiba Naz";
+//  print the message.
+console.log(`Hello ${personName}, would you like to learn some pyton today?`)
