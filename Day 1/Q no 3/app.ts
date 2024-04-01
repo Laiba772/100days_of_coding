0cd95d0362566660a5ee5b94ffb862585c73eb36
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2024-04-01 16:10:32 +0500

    Add files via upload

---
+++ Day 1/Q no 3/app.ts
@@ -0,0 +1,13 @@
+// Assingment 03.
+
+// Store a personName in a veriable.
+let personName :string = "laiba naz";
+
+// Printing a personName in lowercase.
+console.log("Lowercase:" , personName.toLowerCase());
+
+// Printing a personName in Uppercase.
+console.log("Uppercase:" , personName.toUpperCase());
+
+// Printing a personName in Titlecase.
+console.log("Titlecase:" , personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
